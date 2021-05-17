0b8366c7e369287ca76d52b685d1f96dc3ec4510
chosenprogram <me.tahazzot@gmail.com>
2021-05-17 08:04:53 +0600

    17 May 1st Initial

--- src/lib/Helper/DOM.d.ts
+++ src/lib/Helper/DOM.d.ts
@@ -1,7 +1,11 @@
 export default interface DOM {
-    // Element selection
-    select: ( selector: string ) => NodeList | null;
+	// Element selection
+	select: (selector: string) => NodeList | null;
 
-    // Event Listener
-    on: ( event: string, element: HTMLElement | NodeList | string, callback: CallableFunction ) => void;
+	// Event Listener
+	on: (
+		event: string,
+		element: HTMLElement | NodeList | string,
+		callback: CallableFunction
+	) => void;
 }
--- src/lib/Helper/DOM.ts
+++ src/lib/Helper/DOM.ts
@@ -1,45 +1,48 @@
-import InterfaceDOM from './DOM.d'
-import $ from './Helper'
+import InterfaceDOM from "./DOM.d";
+import $ from "./Helper";
 
 const DOM: InterfaceDOM = {
+	// selection
+	select: (selector: string) => {
+		try {
+			return document.querySelectorAll(selector);
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
 
-    // selection
-    select: (selector: string) => {
-        try {
-            return document.querySelectorAll(selector)
-        } catch (err) {
-            throw new Error(err)
-        }
-    },
+	on: (
+		event: string,
+		element: string | HTMLElement | NodeList,
+		callback: Function
+	) => {
+		if ($.isString(element)) {
+			//@ts-ignore
+			document.querySelectorAll(element).forEach((elm) => {
+				//@ts-ignore
+				elm.addEventListener(event, callback);
+			});
 
-    on: (event: string, element: string | HTMLElement | NodeList, callback: Function) => {
-        if ($.isString(element)) {
-            //@ts-ignore
-            document.querySelectorAll(element).forEach(elm => {
-                //@ts-ignore
-                elm.addEventListener(event, callback)
-            })
+			return false;
+		}
 
-            return false;
-        }
+		if (element instanceof HTMLElement) {
+			//@ts-ignore
+			element.addEventListener(event, callback, false);
+			return false;
+		}
 
-        if (element instanceof HTMLElement) {
-            //@ts-ignore
-            element.addEventListener(event, callback, false)
-            return false;
-        }
+		if (element instanceof NodeList) {
+			//@ts-ignore
+			element.forEach((elm) => {
+				//@ts-ignore
+				elm.addEventListener(event, callback, false);
+			});
+			return false;
+		}
 
-        if (element instanceof NodeList) {
-            //@ts-ignore
-            element.forEach(elm => {
-                //@ts-ignore
-                elm.addEventListener(event, callback, false)
-            })
-            return false;
-        }
-
-        throw new Error('Something bad is happening here')
-    }
+		throw new Error("Something bad is happening here");
+	},
 };
 
 export default DOM;
