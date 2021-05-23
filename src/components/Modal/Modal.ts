c77f1fe5e23b12c50a25f00110d3e28b4459763e
chosenprogram <me.tahazzot@gmail.com>
2021-05-23 09:15:23 +0600

    initial commit on 23 May

---
+++ src/components/Modal/Modal.ts
@@ -0,0 +1,10 @@
+// @updated 19 May, 21
+let //get buttons
+	modalButtons: NodeList = document.querySelectorAll('[data-toggle="modal"]');
+
+// Set Event
+modalButtons.forEach((elm) => {
+	elm.addEventListener("click", () => {
+		let target: HTMLElement | null = document.querySelector("");
+	});
+});
